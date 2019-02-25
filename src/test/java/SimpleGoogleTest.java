fead7d94e714c3b286d89583cc884e7f46d7094f
yaroslav <sparrow@mail.ua>
2019-02-25 13:59:51 +0200

    Added examples from session3

--- src/test/java/SimpleGoogleTest.java
+++ src/test/java/SimpleGoogleTest.java
@@ -1,6 +1,8 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.HasCapabilities;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,25 +12,26 @@ public class SimpleGoogleTest {
     WebDriver drv;
 
     @Before
-    public void googleTest_setup(){
+    public void googleTest_setup() {
         WebDriverManager.chromedriver().setup();
 
         ChromeOptions opt = new ChromeOptions();
-        //opt.setHeadless(true);
-        opt.addArguments("start-fullscreen");
+        opt.setExperimentalOption("w3c", true);
+
         drv = new ChromeDriver(opt);
     }
 
     @After
-    public void googleTest_cleanup(){
+    public void googleTest_cleanup() {
 
         drv.quit();
     }
 
     @Test
-    public void googleTest(){
+    public void googleTest() {
         drv.get("https://google.com");
-
+        drv.findElement(By.name("q")).sendKeys("Selenium" + Keys.ENTER);
+        ;
         System.out.println(((HasCapabilities) drv).getCapabilities());
     }
 }
